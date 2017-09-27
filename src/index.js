c2754c1a3e67e026fe229a8fb438449ea4034913
saroko-dnd <isaroko@mail.ru>
2017-09-27 03:37:07 +0300

    Added implementation for multiply(first, second)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  const firstNumberAsArray = first.split('').map(customParseInt);
+  const secondNumberAsArray = second.split('').map(customParseInt);
+  const resultAsArray = [];
+  let startResultIndex = 0;
+  let bufferArray;
+  let modifierThirdStage = 0;
+  let moreThanTenCheckIndex;
+
+  for(let indexSecond = second.length - 1; indexSecond >= 0; --indexSecond){
+    for(let indexFirst = first.length - 1; indexFirst >= 0; --indexFirst){
+      bufferArray = (firstNumberAsArray[indexFirst] * secondNumberAsArray[indexSecond]).toString().split("").map(customParseInt);//yyyy
+      bufferArray.reverse();
+
+      for (let bufferIndex = 0; bufferIndex < bufferArray.length; ++bufferIndex){
+        while (resultAsArray.length < bufferIndex + 1 + startResultIndex + modifierThirdStage){
+          resultAsArray.push(0);
+        }
+        resultAsArray[bufferIndex + startResultIndex + modifierThirdStage] += bufferArray[bufferIndex];
+
+        moreThanTenCheckIndex = bufferIndex + startResultIndex + modifierThirdStage;
+        while (resultAsArray[moreThanTenCheckIndex]/10 >= 1){
+          if (resultAsArray.length < moreThanTenCheckIndex + 2){
+            resultAsArray.push(0);
+          }
+          resultAsArray[moreThanTenCheckIndex + 1] += Math.floor(resultAsArray[moreThanTenCheckIndex]/10);
+          resultAsArray[moreThanTenCheckIndex] = resultAsArray[moreThanTenCheckIndex] % 10;
+          ++moreThanTenCheckIndex;
+        }
+      }
+      ++modifierThirdStage;
+    }
+    modifierThirdStage = 0;
+    ++startResultIndex;
+  }
+  resultAsArray.reverse();
+  return resultAsArray.join('');
+}
+
+function customParseInt(str){
+  return parseInt(str, 10);
 }
